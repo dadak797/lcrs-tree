0e0adb3d05eb730b1363d937b2c38dc34da431d4
dadak797 <dadak797@gmail.com>
2025-04-12 23:23:58 +0900

    Change to template class

--- lcrs_tree.h
+++ lcrs_tree.h
@@ -4,24 +4,24 @@
 
 // Standard library
 #include <string>
-#include <vector>
 #include <memory>
 #include <functional>
+#include <unordered_map>
+#include <cassert>
+
+// Test
+#include <iostream>
 
 
+template <typename T>
 class TreeNode {
 public:
-    TreeNode(int32_t id, const char* text);
+    TreeNode(int32_t id, const char* label);
     ~TreeNode();
-
-    // Setters
-    // void SetText(const char* text) { m_Text = text; }
-    // void SetUserData(void* data) { m_UserData = data; }
 
     // Getters
     int32_t GetId() const { return m_Id; }
-    const char* GetText() const { return m_Text.c_str(); }
-    // const void* GetUserData() const { return m_UserData; }
+    const char* GetLabel() const { return m_Label.c_str(); }
     const TreeNode* GetParent() const { return m_Parent; }
     const TreeNode* GetLeftChild() const { return m_LeftChild; }
     const TreeNode* GetRightSibling() const { return m_RightSibling; }
@@ -32,47 +32,354 @@ public:
 
 private:
     int32_t m_Id;
-    std::string m_Text;
-    // void* m_UserData;
+    std::string m_Label;
+    std::unique_ptr<T> m_Data;
     TreeNode* m_Parent;
     TreeNode* m_LeftChild;
     TreeNode* m_RightSibling;
     TreeNode* m_LeftSibling;
 
+    // All template instances of LcrsTree will be friends with this class.
+    template <typename U>
     friend class LcrsTree;
 };
 
 
-DECLARE_PTR(LcrsTree)
+DECLARE_TEMPLATE_PTR(LcrsTree)
+template <typename T>
 class LcrsTree {
 public:
-    static LcrsTreeUPtr New(const char* text);
+    static LcrsTreeUPtr<T> New(int32_t id, const char* label, std::unique_ptr<T> data = nullptr);
     ~LcrsTree();
 
     // Getters
-    const TreeNode* GetRoot() const { return m_Root; }
-    const TreeNode* GetNodeById(int32_t id);
-
+    const TreeNode<T>* GetRoot() const { return m_Root; }
+    const TreeNode<T>* GetTreeNodeById(int32_t id) const;
+    const T* GetDataById(int32_t id) const;
+    
     // If the parent is nullptr, the root will be used.
-    TreeNode* InsertItem(const char* text, TreeNode* parent = nullptr);
-    bool DeleteItem(TreeNode* node);
+    TreeNode<T>* InsertItem(int32_t id, const char* label, std::unique_ptr<T> data = nullptr, 
+        TreeNode<T>* parent = nullptr);
+    bool DeleteItem(TreeNode<T>* node);
 
     // Traverse all tree nodes
     // The callback function will be called for each node.
-    void TraverseTree(std::function<void(TreeNode*, void*)> callback, TreeNode* startNode = nullptr, void* userData = nullptr);
+    void TraverseTree(std::function<void(TreeNode<T>*, void*)> callback, 
+        TreeNode<T>* startNode = nullptr, void* userData = nullptr);
+    void TraverseTree(std::function<void(const TreeNode<T>*, void*)> callback, 
+        const TreeNode<T>* startNode = nullptr, void* userData = nullptr) const;
 
 private:
     LcrsTree();
 
-    TreeNode* m_Root;
-    static int32_t s_NextId;
+    TreeNode<T>* m_Root;
 
-    bool createRoot(const char* text);
-    void traverseTreeRecursive(std::function<void(TreeNode*, void*)> callback, TreeNode* node, void* userData);
-    void deleteItemRecursive(TreeNode* node);
-
-public:
-    // Helper function to set the next id for a new node
-    // When the tree is loaded from a file, the next id is set to (the maximum id + 1) in the file.
-    void setNextId();
+    bool createRoot(int32_t id, const char* label, std::unique_ptr<T> data = nullptr);
+    void traverseTreeRecursive(std::function<void(TreeNode<T>*, void*)> callback, 
+        TreeNode<T>* node, void* userData);
+    void traverseTreeRecursive(std::function<void(const TreeNode<T>*, void*)> callback, 
+        const TreeNode<T>* node, void* userData) const;
+    void deleteItemRecursive(TreeNode<T>* node);
+    bool isExistingId(int32_t id) const;
 };
+
+
+// Implementation of TreeNode
+
+template <typename T>
+TreeNode<T>::TreeNode(int32_t id, const char* label)
+    : m_Id(id)
+    , m_Label(label)
+    , m_Parent(nullptr)
+    , m_LeftChild(nullptr)
+    , m_RightSibling(nullptr)
+    , m_LeftSibling(nullptr)
+{
+    assert(label && "Label cannot be empty.");
+}
+
+template <typename T>
+TreeNode<T>::~TreeNode() {
+    // NOTE:
+    // The node links are managed by the LcrsTree class,
+    // so it is not need to rearrange the links here.
+    // The user data is managed by the user,
+    // so it is not need to delete the user data here.
+}
+
+template <typename T>
+int32_t TreeNode<T>::GetChildCount() const {
+    int32_t count = 0;
+    TreeNode* child = m_LeftChild;
+
+    // Traverse the right siblings to count the children
+    while (child != nullptr) {
+        ++count;
+        child = child->m_RightSibling;
+    }
+
+    return count;
+}
+
+template <typename T>
+int32_t TreeNode<T>::GetDepth() const {
+    int32_t depth = 0;
+    TreeNode* parent = m_Parent;
+
+    // Traverse up the tree to count the depth
+    while (parent != nullptr) {
+        ++depth;
+        parent = parent->m_Parent;
+    }
+
+    return depth;
+}
+
+
+// Implementation of LcrsTree
+
+template <typename T>
+LcrsTreeUPtr<T> LcrsTree<T>::New(int32_t id, const char* label, std::unique_ptr<T> data) {
+    if (label == nullptr) {
+        return nullptr;
+    }
+
+    // Create a new LcrsTree instance
+    LcrsTreeUPtr<T> tree = LcrsTreeUPtr<T>(new LcrsTree<T>());
+    if (!tree->createRoot(id, label, std::move(data))) {
+        return nullptr;
+    }
+    
+    return std::move(tree);
+}
+
+template <typename T>
+LcrsTree<T>::LcrsTree()
+    : m_Root(nullptr)
+{
+}
+
+template <typename T>
+LcrsTree<T>::~LcrsTree() {
+    if (m_Root) {
+        // Delete all nodes starting from the root
+        deleteItemRecursive(m_Root);
+        m_Root = nullptr;
+    }
+}
+
+template <typename T>
+bool LcrsTree<T>::createRoot(int32_t id, const char* label, std::unique_ptr<T> data) {
+    assert(m_Root == nullptr && "Root already exists.");
+
+    if (isExistingId(id)) {
+        return false;
+    }
+
+    m_Root = new TreeNode<T>(id, label);
+    if (data) {
+        m_Root->m_Data = std::move(data);
+    }
+
+    return true;
+}
+
+template <typename T>
+TreeNode<T>* LcrsTree<T>::InsertItem(int32_t id, const char* label, 
+    std::unique_ptr<T> data, TreeNode<T>* parent)
+{
+    if (isExistingId(id)) {
+        return nullptr;
+    }
+
+    // If the parent is nullptr, insert item under the root.
+    if (parent == nullptr) {
+        parent = m_Root;
+    }
+
+    TreeNode<T>* newNode = new TreeNode<T>(id, label);
+    if (data) {
+        newNode->m_Data = std::move(data);
+    }
+
+    if (parent->m_LeftChild == nullptr) {
+        // If the parent has no children, set the new node as the left child
+        parent->m_LeftChild = newNode;
+        newNode->m_Parent = parent;
+    }
+    else {
+        // Otherwise, find the rightmost sibling and set the new node as the right sibling
+        TreeNode<T>* sibling = parent->m_LeftChild;
+        while (sibling->m_RightSibling != nullptr) {
+            sibling = sibling->m_RightSibling;
+        }
+        sibling->m_RightSibling = newNode;
+        newNode->m_LeftSibling = sibling;
+        newNode->m_Parent = parent;
+    }
+
+    return newNode;
+}
+
+template <typename T>
+bool LcrsTree<T>::DeleteItem(TreeNode<T>* node) {
+    if (node == nullptr) {
+        return false;
+    }
+
+    if (node == m_Root) {
+        // Cannot delete the root node
+        return false;
+    }
+
+    // Update links
+    TreeNode<T>* parent = node->m_Parent;
+    TreeNode<T>* rightSibling = node->m_RightSibling;
+    TreeNode<T>* leftSibling = node->m_LeftSibling;
+
+    if (parent->m_LeftChild == node) {
+        // If the node is the left child of its parent
+        parent->m_LeftChild = rightSibling;
+    }
+
+    if (leftSibling) {
+        // If the node has a left sibling
+        leftSibling->m_RightSibling = rightSibling;
+    }
+
+    if (rightSibling) {
+        // If the node has a right sibling
+        rightSibling->m_LeftSibling = leftSibling;
+    }
+
+    deleteItemRecursive(node);
+
+    return true;
+}
+
+template <typename T>
+void LcrsTree<T>::TraverseTree(std::function<void(TreeNode<T>*, void*)> callback, 
+    TreeNode<T>* startNode, void* userData)
+{
+    if (callback == nullptr) {
+        return;
+    }
+
+    // If startNode is nullptr, start from the root.
+    if (startNode == nullptr) {
+        startNode = m_Root;
+    }
+
+    // Traverse the tree recursively
+    traverseTreeRecursive(callback, startNode, userData);
+}
+
+template <typename T>
+void LcrsTree<T>::TraverseTree(std::function<void(const TreeNode<T>*, void*)> callback, 
+    const TreeNode<T>* startNode, void* userData) const
+{
+    if (callback == nullptr) {
+        return;
+    }
+
+    // If startNode is nullptr, start from the root.
+    if (startNode == nullptr) {
+        startNode = m_Root;
+    }
+
+    // Traverse the tree recursively
+    traverseTreeRecursive(callback, startNode, userData);
+}
+
+template <typename T>
+void LcrsTree<T>::traverseTreeRecursive(std::function<void(TreeNode<T>*, void*)> callback,
+    TreeNode<T>* node, void* userData)
+{
+    if (!node || !callback) {
+        return;
+    }
+
+    // Call the callback function for the current node
+    callback(node, userData);
+
+    // Traverse all children
+    TreeNode<T>* child = node->m_LeftChild;
+    while (child != nullptr) {
+        traverseTreeRecursive(callback, child, userData);
+        child = child->m_RightSibling;
+    }
+}
+
+template <typename T>
+void LcrsTree<T>::traverseTreeRecursive(std::function<void(const TreeNode<T>*, void*)> callback,
+    const TreeNode<T>* node, void* userData) const
+{
+    if (!node || !callback) {
+        return;
+    }
+
+    // Call the callback function for the current node
+    callback(node, userData);
+
+    // Traverse all children
+    const TreeNode<T>* child = node->m_LeftChild;
+    while (child != nullptr) {
+        traverseTreeRecursive(callback, child, userData);
+        child = child->m_RightSibling;
+    }
+}
+
+template <typename T>
+void LcrsTree<T>::deleteItemRecursive(TreeNode<T>* node) {
+    if (node == nullptr) {
+        return;
+    }
+
+    // Recursively delete all children
+    TreeNode<T>* child = node->m_LeftChild;
+    while (child != nullptr) {
+        TreeNode<T>* nextChild = child->m_RightSibling;
+        deleteItemRecursive(child);
+        child = nextChild;
+    }
+
+    // Delete the current node
+    delete node;
+}
+
+template <typename T>
+const TreeNode<T>* LcrsTree<T>::GetTreeNodeById(int32_t id) const {
+    if (m_Root == nullptr) {
+        return nullptr;
+    }
+
+    const TreeNode<T>* foundNode = nullptr;
+
+    // Traverse the tree to find the node with the specified id
+    traverseTreeRecursive([&foundNode, id](const TreeNode<T>* node, void*) {
+        if (node->GetId() == id) {
+            foundNode = node;
+            return;
+        }
+    }, m_Root, nullptr);
+
+    return foundNode;
+}
+
+template <typename T>
+bool LcrsTree<T>::isExistingId(int32_t id) const {
+    if (GetTreeNodeById(id) != nullptr) {
+        assert(false && "Id already exists.");
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+const T* LcrsTree<T>::GetDataById(int32_t id) const {
+    const TreeNode<T>* node = GetTreeNodeById(id);
+    if (node) {
+        return node->m_Data.get();
+    }
+    return nullptr;
+}
--- main.cpp
+++ main.cpp
@@ -2,78 +2,76 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <unordered_map>
+
+
+class Mesh {
+public:
+    Mesh(int32_t id, const char* name, int32_t nodeCount, int32_t faceCount)
+        : m_Id(id), m_Name(name), m_NodeCount(nodeCount), m_FaceCount(faceCount)
+    {
+        std::cout << "Creating Mesh - [Id]: " << m_Id << std::endl;
+    }
+    
+    ~Mesh() {
+        std::cout << "Destroying Mesh - [Id]: " << m_Id << std::endl;
+    }
+
+    const int32_t GetId() const { return m_Id; }
+    const char* GetName() const { return m_Name.c_str(); }
+    const int32_t GetNodeCount() const { return m_NodeCount; }
+    const int32_t GetFaceCount() const { return m_FaceCount; }
+
+private:
+    int32_t m_Id;
+    std::string m_Name;
+    int32_t m_NodeCount;
+    int32_t m_FaceCount;
+};
 
 
 int main() {
-    LcrsTreeUPtr geomTree = LcrsTree::New("0");
-    if (!geomTree) {
-        std::cerr << "Failed to create LCRS tree." << std::endl;
-        return -1;
-    }
+    std::unique_ptr<Mesh> pMesh0 = std::make_unique<Mesh>(0, "Mesh0", 100, 200);
+    LcrsTreeUPtr<Mesh> geomTree = LcrsTree<Mesh>::New(pMesh0->GetId(), pMesh0->GetName(), std::move(pMesh0));
 
-    // Insert items into the tree
-    TreeNode* node01 = geomTree->InsertItem("0-1");
-    TreeNode* node02 = geomTree->InsertItem("0-2");
-    TreeNode* node03 = geomTree->InsertItem("0-3");
-    TreeNode* node11 = geomTree->InsertItem("1-1", node01);
-    TreeNode* node12 = geomTree->InsertItem("1-2", node01);
-    TreeNode* node21 = geomTree->InsertItem("2-1", node02);
-    TreeNode* node22 = geomTree->InsertItem("2-2", node02);
-    TreeNode* node31 = geomTree->InsertItem("3-1", node03);
-    TreeNode* node32 = geomTree->InsertItem("3-2", node03);
-    TreeNode* node33 = geomTree->InsertItem("3-3", node03);
-    TreeNode* node111 = geomTree->InsertItem("1-1-1", node11);
-    TreeNode* node112 = geomTree->InsertItem("1-1-2", node11);
+    std::unique_ptr<Mesh> pMesh1 = std::make_unique<Mesh>(1, "Mesh1", 150, 250);
+    std::unique_ptr<Mesh> pMesh11 = std::make_unique<Mesh>(2, "Mesh11", 200, 300);
+    std::unique_ptr<Mesh> pMesh12 = std::make_unique<Mesh>(3, "Mesh12", 250, 350);
+    std::unique_ptr<Mesh> pMesh2 = std::make_unique<Mesh>(4, "Mesh2", 300, 400);
+    std::unique_ptr<Mesh> pMesh3 = std::make_unique<Mesh>(5, "Mesh3", 350, 450);
 
-    geomTree->TraverseTree([](TreeNode* node, void* userData) {
-        for (int i = 0; i < node->GetDepth(); i++) {
+    TreeNode<Mesh>* node1 = geomTree->InsertItem(pMesh1->GetId(), pMesh1->GetName(), std::move(pMesh1));
+    geomTree->InsertItem(pMesh11->GetId(), pMesh11->GetName(), std::move(pMesh11), node1);
+    geomTree->InsertItem(pMesh12->GetId(), pMesh12->GetName(), std::move(pMesh12), node1);
+    TreeNode<Mesh>* node2 = geomTree->InsertItem(pMesh2->GetId(), pMesh2->GetName(), std::move(pMesh2));
+    TreeNode<Mesh>* node3 = geomTree->InsertItem(pMesh3->GetId(), pMesh3->GetName(), std::move(pMesh3));
+
+    geomTree->TraverseTree([](TreeNode<Mesh>* node, void* userData) {
+        for (int i = 0; i < node->GetDepth(); ++i) {
             std::cout << "--";
         }
 
-        std::cout << "[Node]: " << node->GetText() << ", [ID]: " << node->GetId();
-        std::cout << ", [Parent]: " << (node->GetParent() ? node->GetParent()->GetText() : "NULL");
-        std::cout << ", [Left Child]: " << (node->GetLeftChild() ? node->GetLeftChild()->GetText(): "NULL");
-        std::cout << ", [Right Sibling]: " << (node->GetRightSibling() ? node->GetRightSibling()->GetText() : "NULL");
-        std::cout << ", [Left Sibling]: " << (node->GetLeftSibling() ? node->GetLeftSibling()->GetText() : "NULL");
+        std::cout << "[Node]: " << node->GetLabel() << ", [ID]: " << node->GetId();
+        std::cout << ", [Parent]: " << (node->GetParent() ? node->GetParent()->GetLabel() : "NULL");
+        std::cout << ", [Left Child]: " << (node->GetLeftChild() ? node->GetLeftChild()->GetLabel(): "NULL");
+        std::cout << ", [Right Sibling]: " << (node->GetRightSibling() ? node->GetRightSibling()->GetLabel() : "NULL");
+        std::cout << ", [Left Sibling]: " << (node->GetLeftSibling() ? node->GetLeftSibling()->GetLabel() : "NULL");
         std::cout << std::endl;
     });
 
-    const TreeNode* node = geomTree->GetNodeById(11);
-    if (node) {
-        std::cout << "Found node with Id 11: " << node->GetText() << std::endl;
-    } else {
-        std::cout << "Node with Id 11 not found." << std::endl;
-    }
+    geomTree->DeleteItem(node1);
 
-    geomTree->DeleteItem(node11);
-    std::cout << "--- After deleting node11 ---" << std::endl;
-
-    geomTree->TraverseTree([](TreeNode* node, void* userData) {
-        for (int i = 0; i < node->GetDepth(); i++) {
+    std::cout << "After deleting node1:" << std::endl;
+    geomTree->TraverseTree([](TreeNode<Mesh>* node, void* userData) {
+        for (int i = 0; i < node->GetDepth(); ++i) {
             std::cout << "--";
         }
 
-        std::cout << "[Node]: " << node->GetText() << ", [ID]: " << node->GetId();
-        std::cout << ", [Parent]: " << (node->GetParent() ? node->GetParent()->GetText() : "NULL");
-        std::cout << ", [Left Child]: " << (node->GetLeftChild() ? node->GetLeftChild()->GetText(): "NULL");
-        std::cout << ", [Right Sibling]: " << (node->GetRightSibling() ? node->GetRightSibling()->GetText() : "NULL");
-        std::cout << ", [Left Sibling]: " << (node->GetLeftSibling() ? node->GetLeftSibling()->GetText() : "NULL");
-        std::cout << std::endl;
-    });
-
-    geomTree->DeleteItem(node33);
-    std::cout << "--- After deleting node33 ---" << std::endl;
-
-    geomTree->TraverseTree([](TreeNode* node, void* userData) {
-        for (int i = 0; i < node->GetDepth(); i++) {
-            std::cout << "--";
-        }
-
-        std::cout << "[Node]: " << node->GetText() << ", [ID]: " << node->GetId();
-        std::cout << ", [Parent]: " << (node->GetParent() ? node->GetParent()->GetText() : "NULL");
-        std::cout << ", [Left Child]: " << (node->GetLeftChild() ? node->GetLeftChild()->GetText(): "NULL");
-        std::cout << ", [Right Sibling]: " << (node->GetRightSibling() ? node->GetRightSibling()->GetText() : "NULL");
-        std::cout << ", [Left Sibling]: " << (node->GetLeftSibling() ? node->GetLeftSibling()->GetText() : "NULL");
+        std::cout << "[Node]: " << node->GetLabel() << ", [ID]: " << node->GetId();
+        std::cout << ", [Parent]: " << (node->GetParent() ? node->GetParent()->GetLabel() : "NULL");
+        std::cout << ", [Left Child]: " << (node->GetLeftChild() ? node->GetLeftChild()->GetLabel(): "NULL");
+        std::cout << ", [Right Sibling]: " << (node->GetRightSibling() ? node->GetRightSibling()->GetLabel() : "NULL");
+        std::cout << ", [Left Sibling]: " << (node->GetLeftSibling() ? node->GetLeftSibling()->GetLabel() : "NULL");
         std::cout << std::endl;
     });
 
